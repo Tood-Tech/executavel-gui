c2e795522ab249fde39ae732788846f89cacd29b
samuel-lima-ribeiro <samuel.ribeiro@sptech.school>
2023-05-31 01:34:37 -0300

    finalizando

--- scriptBanco.sh
+++ scriptBanco.sh
@@ -8,7 +8,7 @@ sudo docker build -t dockerfile -f Dockerfile-mysql .
 
 sudo docker run -d -p 3306:3306 --name ContainerBD -e MYSQL_ROOT_PASSWORD=sptech -e MYSQL_DATABASE=ToodDatabase dockerfile
 
-sleep 2
+sleep 5
 
-sudo chmod 777 ToodInterface.jar
-java -jar ToodInterface.jar
+sudo chmod 777 login-tood-java-swing-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar login-tood-java-swing-1.0-SNAPSHOT-jar-with-dependencies.jar
